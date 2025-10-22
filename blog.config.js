a38c9b689c07c293443fc18086e3ad13c5e0abbc
bgrj <hsz@bgrj.edu.pl>
2025-10-23 06:37:05 +0800

    Update blog.config.js

--- blog.config.js
+++ blog.config.js
@@ -17,7 +17,7 @@ const BLOG = {
   APPEARANCE_DARK_TIME: process.env.NEXT_PUBLIC_APPEARANCE_DARK_TIME || [18, 6], // 夜间模式起至时间，false时关闭根据时间自动切换夜间模式
 
   AUTHOR: process.env.NEXT_PUBLIC_AUTHOR || '不过人间', // 您的昵称 例如 tangly1024
-  BIO: process.env.NEXT_PUBLIC_BIO || '终生追求自我解放的无知者', // 作者简介
+  BIO: process.env.NEXT_PUBLIC_BIO || '一个终生追求自我解放并成为我之所是的无知者', // 作者简介
   LINK: process.env.NEXT_PUBLIC_LINK || 'https://bgrj.edu.pl', // 网站地址
   KEYWORDS: process.env.NEXT_PUBLIC_KEYWORD || '不过人间', // 网站关键词 英文逗号隔开
   BLOG_FAVICON: process.env.NEXT_PUBLIC_FAVICON || '/favicon.ico', // blog favicon 配置, 默认使用 /public/favicon.ico，支持在线图片，如 https://img.imesong.com/favicon.png
