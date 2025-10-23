1f3b809160bb31cbc6a8487c6fc046d8fd179196
bgrj <hsz@bgrj.edu.pl>
2025-10-23 23:26:20 +0800

    Update contact.config.js

--- conf/contact.config.js
+++ conf/contact.config.js
@@ -9,7 +9,8 @@ module.exports = {
         unescape(encodeURIComponent(process.env.NEXT_PUBLIC_CONTACT_EMAIL))
       )) ||
     'Ymdyai5leGlzdGVudGlhbC50aGlua2luZ0BnbWFpbC5jb20=', // 邮箱地址 例如mail@tangly1024.com
-  CONTACT_TELEGRAM: process.env.NEXT_PUBLIC_CONTACT_TELEGRAM || 'https://t.me/+G_DTb1YKTYs3MDE1', // 你的telegram 地址 例如 https://t.me/tangly_1024
+  CONTACT_TELEGRAM: process.env.NEXT_PUBLIC_CONTACT_TELEGRAM || 'https://t.me/+G_DTb1YKTYs3MDE1', // 电报群
   CONTACT_THREADS: process.env.NEXT_PUBLIC_CONTACT_THREADS || 'https://www.threads.com/@bu.guo.ren.jian', // 脆主页
+  CONTACT_WHATSAPP: process.env.NEXT_PUBLIC_CONTACT_WHATSAPP || 'https://chat.whatsapp.com/BsQWGqCYYaTCw7khlsq4gx?mode=wwc', // Whatsapp群
   CONTACT_WEHCHAT_PUBLIC: process.env.NEXT_PUBLIC_CONTACT_WEHCHAT_PUBLIC || 'https://mp.weixin.qq.com/mp/profile_ext?action=home&__biz=Mzk0MDQ5NTg3Nw==#wechat_redirect' // 微信公众号 格式：https://mp.weixin.qq.com/mp/profile_ext?action=home&__biz=【xxxxxx】==#wechat_redirect
 }
