9d9eff5f9fc72ac7edac0f198389422590f78f28
bgrj <hsz@bgrj.edu.pl>
2025-10-26 04:42:45 +0800

    Update contact.config.js

--- conf/contact.config.js
+++ conf/contact.config.js
@@ -12,5 +12,5 @@ module.exports = {
   CONTACT_TELEGRAM: process.env.NEXT_PUBLIC_CONTACT_TELEGRAM || 'https://t.me/+G_DTb1YKTYs3MDE1', // 电报群
   CONTACT_THREADS: process.env.NEXT_PUBLIC_CONTACT_THREADS || 'https://www.threads.com/@bu.guo.ren.jian', // 脆主页
   CONTACT_WHATSAPP: process.env.NEXT_PUBLIC_CONTACT_WHATSAPP || 'https://chat.whatsapp.com/BsQWGqCYYaTCw7khlsq4gx?mode=wwc', // Whatsapp群
-  CONTACT_WEHCHAT_PUBLIC: process.env.NEXT_PUBLIC_CONTACT_WEHCHAT_PUBLIC || 'https://mp.weixin.qq.com/mp/profile_ext?action=home&__biz=Mzk0MDQ5NTg3Nw==#wechat_redirect' // 微信公众号 格式：https://mp.weixin.qq.com/mp/profile_ext?action=home&__biz=【xxxxxx】==#wechat_redirect
+  CONTACT_WEHCHAT_PUBLIC: process.env.NEXT_PUBLIC_CONTACT_WEHCHAT_PUBLIC || '' // 微信公众号 格式：https://mp.weixin.qq.com/mp/profile_ext?action=home&__biz=【xxxxxx】==#wechat_redirect
 }
