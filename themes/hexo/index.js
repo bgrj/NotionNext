cacd0476d99e2102304607740a9b7d8c02a23f4c
bgrj <hsz@bgrj.edu.pl>
2025-10-25 12:13:54 +0800

    Update index.js

--- themes/hexo/index.js
+++ themes/hexo/index.js
@@ -141,11 +141,11 @@ const LayoutBase = props => {
             </div>
 
             {/* 右侧栏 */}
-            <SideRight {...props} className="ml-[-80px]" /> {/* 通过负的左外边距让整个右侧栏左移 */}
+            <SideRight {...props} className="ml-[-100px]" /> {/* 通过负的左外边距让整个右侧栏左移 */}
           </div>
         </main>
 
-        <div className='block lg:hidden ml-[-80px]'>
+        <div className='block lg:hidden ml-[-100px]'>
           <TocDrawer post={post} cRef={drawerRight} targetRef={tocRef} />
         </div>
 
