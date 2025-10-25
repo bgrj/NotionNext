a585400f9ab3ff3d7bb3555b2bafad697ce3bc32
bgrj <hsz@bgrj.edu.pl>
2025-10-26 06:04:56 +0800

    Update Hero.js

--- themes/hexo/components/Hero.js
+++ themes/hexo/components/Hero.js
@@ -78,7 +78,7 @@ const Hero = props => {
         {/* 滚动按钮 */}
         <div
           onClick={scrollToWrapper}
-          className='z-10 cursor-pointer w-full text-center py-4 text-3xl absolute bottom-10 text-white'>
+          className='z-10 cursor-pointer w-full text-center py-4 text-3xl absolute bottom-10 text-white shadow-text'>
           <div className='opacity-70 animate-bounce text-xs'>
             {siteConfig('HEXO_SHOW_START_READING', null, CONFIG) &&
               locale.COMMON.START_READING}
