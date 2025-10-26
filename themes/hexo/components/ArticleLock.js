15c71d69548b40f4f22050ddfad27224ed107bad
bgrj <hsz@bgrj.edu.pl>
2025-10-26 09:32:17 +0800

    Update ArticleLock.js

--- themes/hexo/components/ArticleLock.js
+++ themes/hexo/components/ArticleLock.js
@@ -27,7 +27,7 @@ export const ArticleLock = props => {
     passwordInputRef.current.focus()
   }, [])
 
-  return <div id='container' className='w-full flex pl-64 items-center h-96 '>
+  return <div id='container' className='w-full flex items-center h-96 '>
     <div className='text-center space-y-3'>
       <div className='font-bold dark:text-gray-300 text-black'>{locale.COMMON.ARTICLE_LOCK_TIPS}</div>
       <div className='flex mx-4'>
