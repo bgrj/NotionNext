5e427909648e1b7fdcc814db34ed59bbc1ccd786
bgrj <hsz@bgrj.edu.pl>
2025-11-24 03:40:03 +0800

    Update PoweredBy.js

--- components/PoweredBy.js
+++ components/PoweredBy.js
@@ -1,13 +1,13 @@
 import { siteConfig } from '@/lib/config'
 
 /**
- * 驱动版权
+ * 驱动版权（改为了如下"内容写作"）
  * @returns
  */
 export default function PoweredBy(props) {
   return (
     <div className={`inline text-sm font-serif ${props.className || ''}`}>
-      <span className='mr-1'>Powered by</span>
+      <span className='mr-1'>Written by</span>
       
       {/* 修改点：
           1. 把 <a> 改成了 <span>
@@ -15,7 +15,7 @@ export default function PoweredBy(props) {
           3. 删掉了 className 里的 'underline' (下划线)
       */}
       <span className='justify-start'>
-        NotionNext
+        不过人间·EのT
       </span>
       .
     </div>
