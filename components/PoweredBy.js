9d20af89c246071b38ff4a72a4deed3d0d39e1bb
bgrj <hsz@bgrj.edu.pl>
2025-11-24 03:18:40 +0800

    Update PoweredBy.js

--- components/PoweredBy.js
+++ components/PoweredBy.js
@@ -8,11 +8,15 @@ export default function PoweredBy(props) {
   return (
     <div className={`inline text-sm font-serif ${props.className || ''}`}>
       <span className='mr-1'>Powered by</span>
-      <a
-        href='/' 
-        className='underline justify-start'>
+      
+      {/* 修改点：
+          1. 把 <a> 改成了 <span>
+          2. 删掉了 href="..."
+          3. 删掉了 className 里的 'underline' (下划线)
+      */}
+      <span className='justify-start'>
         不过人间·EのT
-      </a>
+      </span>
       .
     </div>
   )
