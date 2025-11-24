1c6d0fb96378793d52f39e9113fffe9f0c588d61
bgrj <hsz@bgrj.edu.pl>
2025-11-25 04:16:37 +0800

    Update AnalyticsCard.js

--- themes/hexo/components/AnalyticsCard.js
+++ themes/hexo/components/AnalyticsCard.js
@@ -1,57 +1,35 @@
 import Card from './Card'
 
 export function AnalyticsCard (props) {
-
   const { postCount } = props
-
-  return <Card>
-
-    <div className='ml-2 mb-3 '>
-
-      <i className='fas fa-chart-area' /> 统计
-
-    </div>
-
-    <div className='text-xs  font-light justify-center mx-7'>
-
-      <div className='inline'>
-
-        <div className='flex justify-between'>
-
-          <div>文章数:</div>
-
-          <div>{postCount}</div>
-
-        </div>
-
+  return (
+    <Card>
+      <div className='ml-2 mb-3'>
+        <i className='fas fa-chart-area' /> 统计
       </div>
-
-      <div className='hidden busuanzi_container_page_pv ml-2'>
-
-        <div className='flex justify-between'>
-
-          <div>访问量:</div>
-
-          <div className='busuanzi_value_page_pv' />
-
+      <div className='text-xs font-light justify-center mx-7'>
+        <div className='inline'>
+          <div className='flex justify-between'>
+            <div>文章数:</div>
+            <div>{postCount}</div>
+          </div>
         </div>
-
-      </div>
-
-      <div className='hidden busuanzi_container_site_uv ml-2'>
-
-        <div className='flex justify-between'>
-
-          <div>访客数:</div>
-
-          <div className='busuanzi_value_site_uv' />
-
+        {/* 访问量 (Page PV) */}
+        <div className='hidden busuanzi_container_page_pv ml-2'>
+          <div className='flex justify-between'>
+            <div>访问量:</div>
+            <div className='busuanzi_value_page_pv' />
+          </div>
         </div>
-
+        {/* 访客数 (Site UV) */}
+        <div className='hidden busuanzi_container_site_uv ml-2'>
+          <div className='flex justify-between'>
+            <div>访客数:</div>
+            <div className='busuanzi_value_site_uv' />
+          </div>
+        </div>
       </div>
-
-    </div>
-
-  </Card>
-
+    </Card>
+  )
 }
+export default AnalyticsCard
